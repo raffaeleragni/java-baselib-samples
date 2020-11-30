f24e2fe546dad424071ba06a695c556811b1a4ba
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-11-30 06:59:08 +0100

    updated samples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import baselib.HttpServer;
-import baselib.JSONBuilder;
+import baselib.HttpServer.Context;
+import static baselib.JSONBuilder.toJSON;
+import static baselib.JSONReader.toRecord;
 import baselib.JdbcInstance;
 import static java.lang.String.valueOf;
 import java.util.Map;
-import java.util.UUID;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
   public static void main(String[] args) {
@@ -13,25 +14,32 @@ public class Main {
 
   public record Table(String id, String name) {}
 
-  void main() {
+  JdbcInstance prepareDB() {
     System.setProperty("JDBC_URL", "jdbc:h2:mem:shenanigans;DB_CLOSE_DELAY=-1");
     var db = JdbcInstance.defaultClient();
     db.execute("create table if not exists test (id varchar(255), name varchar(255), primary key(id))", st -> {});
+    return db;
+  }
 
-    var selector = db.makeRecordSelector(Table.class, "select id,name from test", st -> {});
-    BiFunction<String, String, String> addNew = (id, name) -> valueOf(
-        db.execute("insert into test (id, name) values(?, ?)", st -> {
-          st.setString(1, id);
-          st.setString(2, name);
-        }));
-
-    HttpServer.create(8080, HttpServer.of(Map.of(
-      "/", c -> "test",
-      "/get", c -> JSONBuilder.toJSON(selector.get()),
-      "/insert", c -> addNew.apply(UUID.randomUUID().toString(), c.variablePath()),
-      "/echo", c -> c.body()
-    ))).start();
-
+  void runServer(Map<String, Function<Context, String>> endpoints) {
+    HttpServer.create(8080, HttpServer.of(endpoints)).start();
     System.out.println("Started server on port 8080");
+  }
+
+  void main() {
+    var db = prepareDB();
+    var selector = db.makeRecordSelector(Table.class, "select id,name from test", st -> {});
+    Function<Table, String> addNew = t -> valueOf(
+      db.execute("insert into test (id, name) values(?, ?)", st -> {
+        st.setString(1, t.id());
+        st.setString(2, t.name());
+      }));
+
+    runServer(Map.of(
+      "/", c -> "test",
+      "/get", c -> toJSON(selector.get()),
+      "/insert", c -> addNew.apply(toRecord(Table.class, c.body())),
+      "/echo", c -> c.body()
+    ));
   }
 }
